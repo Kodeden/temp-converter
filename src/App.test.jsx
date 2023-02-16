e5ceedd1bf20df8938606aaf3d1985d587369961
Manav Misra <manavm@visionwebsoft.com>
2023-02-15 18:11:35 -0600

    Add failing tests

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,26 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { expect } from "vitest";
 import App from "./App";
-import { expect, test } from "vitest";
 
-test("renders correctly", () => {
+it("converts °C to °F", async () => {
   render(<App />);
-  expect(screen.getByText("Hello world!")).toBeInTheDocument();
+  const inputCelsius = screen.getByLabelText(/Celsius/i);
+  const inputFahrenheit = screen.getByLabelText(/Fahrenheit/i);
+  const user = userEvent.setup();
+
+  await user.type(inputCelsius, "100");
+
+  expect(inputFahrenheit).toHaveValue(212);
+});
+
+it("converts °F to °C", async () => {
+  render(<App />);
+  const inputCelsius = screen.getByLabelText(/Celsius/i);
+  const inputFahrenheit = screen.getByLabelText(/Fahrenheit/i);
+  const user = userEvent.setup();
+
+  await user.type(inputFahrenheit, "212");
+
+  expect(inputCelsius).toHaveValue(100);
 });
